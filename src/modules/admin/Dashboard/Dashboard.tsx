2c742ff726f6393402c1f4de59dfd9b8811af070
Viết Minh Hiếu <hieuvm1@vmodev.com>
2022-09-05 15:14:21 +0700

    demo commitlint

--- src/modules/admin/Dashboard/Dashboard.tsx
+++ src/modules/admin/Dashboard/Dashboard.tsx
@@ -1,7 +1,5 @@
 const Dashboard = () => (
-    <div className="text-3xl font-light text-red-800 underline">
-      Dashboard 1
-    </div>
-  );
+  <div className="text-3xl font-light text-red-800 underline">Dashboard</div>
+);
 
 export default Dashboard;
